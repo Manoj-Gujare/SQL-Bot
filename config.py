16ca8f24ae351d45f023fe745b6e6ce17d2b3e90
Manoj Gujare <manojgujare726@gmail.com>
2025-01-20 12:20:34 +0530

    Implement centralized configuration file for managing app settings and constants

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_SETTINGS = {
+    "host": "localhost",
+    "port": "3306",
+    "user": "root",
+    "password": "root",
+    "database": "classicmodels",
+}
